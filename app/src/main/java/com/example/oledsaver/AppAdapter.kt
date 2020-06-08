766eaeac8e957226d138127b8f08156578a532c6
Carlos Catly <ccatly@gmail.com>
2020-06-07 21:32:24 -0700

    implementing overriden functions

--- app/src/main/java/com/example/oledsaver/AppAdapter.kt
+++ app/src/main/java/com/example/oledsaver/AppAdapter.kt
@@ -5,47 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.oledsaver.app.AppListItem
 
-class AppAdapter(context: Context, customizedListView:List<AppListItem>):BaseAdapter() {
-    private val layoutInflater:LayoutInflater
-    private val listStorage:List<AppListItem>
-    val count:Int
-        get() {
-            return listStorage.size
-        }
-    init{
+class AppAdapter(var layoutInflater: LayoutInflater, var listStorage: List<AppListItem>, context: Context, customizedListView: List<AppListItem>) : BaseAdapter() {
+    init {
         layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         listStorage = customizedListView
     }
-    fun getItem(position:Int):Any {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Any {
         return position
     }
-    fun getItemId(position:Int):Long {
-        return position.toLong()
+
+    override fun getCount(): Int {
+        return listStorage.size
     }
-    fun getView(position:Int, convertView: View, parent: ViewGroup):View {
-        val listViewHolder:ViewHolder
-        if (convertView == null)
-        {
-            listViewHolder = ViewHolder()
-            convertView = layoutInflater.inflate(R.layout.installed_app_list, parent, false)
-            listViewHolder.textInListView = convertView.findViewById(R.id.list_app_name) as TextView
-            listViewHolder.imageInListView = convertView.findViewById(R.id.app_icon) as ImageView
-            convertView.setTag(listViewHolder)
-        }
-        else
-        {
-            listViewHolder = convertView.getTag() as ViewHolder
-        }
-        listViewHolder.textInListView.setText(listStorage.get(position).getName())
-        listViewHolder.imageInListView.setImageDrawable(listStorage.get(position).getIcon())
-        return convertView
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
     }
-    internal class ViewHolder {
-        var textInListView:TextView
-        var imageInListView:ImageView
-    }
+
 }
