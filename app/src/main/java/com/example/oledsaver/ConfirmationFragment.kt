3fcb073ba5a3a24dfd5f3f423939944058475db5
Carlos Catly <ccatly@gmail.com>
2020-06-14 23:00:49 -0700

    added layout

--- app/src/main/java/com/example/oledsaver/ConfirmationFragment.kt
+++ app/src/main/java/com/example/oledsaver/ConfirmationFragment.kt
@@ -1,4 +1,27 @@
 package com.example.oledsaver
 
-class ConfirmationFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ConfirmationFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        fab.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+
+    }
 }
