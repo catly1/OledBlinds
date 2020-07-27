30d5baa12dd0666b698484b4fc9ed83656ba1e81
Carlos Catly <ccatly@gmail.com>
2020-07-26 22:16:27 -0700

    still working on the getview function for the saved settings

--- app/src/main/java/com/example/oledsaver/adapter/SavedAppAdapter.kt
+++ app/src/main/java/com/example/oledsaver/adapter/SavedAppAdapter.kt
@@ -1,15 +1,27 @@
 package com.example.oledsaver.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import com.example.oledsaver.R
 import com.example.oledsaver.entity.Setting
 
-class SavedAppAdapter(private val allSavedSettings: List<Setting>) : BaseAdapter() {
+class SavedAppAdapter(context: Context?, private val allSavedSettings: List<Setting>) : BaseAdapter() {
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
 
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         var listViewHolder : ViewHolder
+        val view: View?
+
+        if(convertView == null){
+            view = layoutInflater.inflate(R.layout.app_list_item, parent, false)
+        } else {
+            view = convertView
+        }
+
+        return view
     }
 
     override fun getItem(position: Int): Any {
