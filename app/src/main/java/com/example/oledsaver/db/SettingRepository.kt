605b707c281c11934f83c8967be36c043fe1ebb4
Carlos Catly <ccatly@gmail.com>
2020-08-05 21:20:37 -0700

    values assigned on an executor block cannot be returned?

--- app/src/main/java/com/example/oledsaver/db/SettingRepository.kt
+++ app/src/main/java/com/example/oledsaver/db/SettingRepository.kt
@@ -2,6 +2,7 @@ package com.example.oledsaver.db
 
 import com.example.oledsaver.entity.Setting
 import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,6 +13,18 @@ class SettingRepository @Inject constructor(
     ) {
 
     fun getSettings(): List<Setting> {
+
         return settingDao.getAll()
+    }
+
+    private fun test(){
+        executor.execute{
+            val list: List<Setting> = settingDao.getAll()
+        }
+
+    }
+
+    companion object {
+        val FRESH_TIMEOUT = TimeUnit.DAYS.toMillis(1)
     }
 }
