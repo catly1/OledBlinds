bf422cfa9609af7049a4b04fe32581291476cdc0
Carlos Catly <ccatly@gmail.com>
2020-08-07 21:41:37 -0700

    refactoring for db livedata return

--- app/src/main/java/com/example/oledsaver/db/SettingRepository.kt
+++ app/src/main/java/com/example/oledsaver/db/SettingRepository.kt
@@ -2,8 +2,6 @@ package com.example.oledsaver.db
 
 import androidx.lifecycle.LiveData
 import com.example.oledsaver.entity.Setting
-import java.util.concurrent.Executor
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,16 +10,8 @@ class SettingRepository @Inject constructor(
     private val settingDao: SettingDao
     ) {
 
-    fun getSettings(): List<Setting> {
-        println("before crash")
-        var test = settingDao.testAll()
-        println("test works")
-        println(test.value)
-        var list: List<Setting> = settingDao.getAll()
-        println("after crash?")
-        println(list)
-
-        return list
+    fun getSettings(): LiveData<List<Setting>> {
+        return settingDao.getAll()
     }
 
 
