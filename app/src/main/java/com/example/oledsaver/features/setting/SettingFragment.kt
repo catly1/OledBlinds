b296c757a86ea93f32041bb909286f3364955faa
Carlos Catly <ccatly@gmail.com>
2020-08-14 22:29:19 -0700

    added the oncreateview for the setting fragment

--- app/src/main/java/com/example/oledsaver/features/setting/SettingFragment.kt
+++ app/src/main/java/com/example/oledsaver/features/setting/SettingFragment.kt
@@ -1,7 +1,17 @@
 package com.example.oledsaver.features.setting
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.oledsaver.R
 
 class SettingFragment : Fragment() {
-    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return  inflater.inflate(R.layout.fragment_setting, container, false)
+    }
 }
