1274576fa5ce6af98e61fdc43d0e052f8a1923e2
Carlos Catly <ccatly@gmail.com>
2020-08-18 21:19:22 -0700

    added a button in the floating menu layout and starting its service

---
+++ app/src/main/java/com/example/oledsaver/features/floating_menu/FloatingMenuService.kt
@@ -0,0 +1,11 @@
+package com.example.oledsaver.features.floating_menu
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FloatingMenuService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
