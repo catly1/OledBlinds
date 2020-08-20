3f768fe8998c69f439671f38b60250255f73908a
Carlos Catly <ccatly@gmail.com>
2020-08-20 16:30:29 -0700

    finished touch override for floating menu. Dunno if it works lol

--- app/src/main/java/com/example/oledsaver/features/floating_menu/FloatingMenuService.kt
+++ app/src/main/java/com/example/oledsaver/features/floating_menu/FloatingMenuService.kt
@@ -1,15 +1,14 @@
 package com.example.oledsaver.features.floating_menu
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.IBinder
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.WindowManager
+import android.view.*
 import android.widget.Button
 import com.example.oledsaver.R
+import com.example.oledsaver.features.main.MainActivity
 
 class FloatingMenuService: Service() {
     private lateinit var mWindowManager:WindowManager
@@ -18,6 +17,7 @@ class FloatingMenuService: Service() {
         return null
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate() {
         super.onCreate()
         //Inflate the chat head layout we created
@@ -44,20 +44,61 @@ class FloatingMenuService: Service() {
         }
 
         val test = floatingMenuView.findViewById<Button>(R.id.float_button)
-        test.setOnClickListener {
-            var lastAction : Int
-            var initialX : Int
-            var initialY: Int
-            var initialTouchX : Float
-            var initialTouchY : Float
+        test.setOnTouchListener (object : View.OnTouchListener {
+            var lastAction : Int = 0
+            var initialX : Int = 0
+            var initialY: Int = 0
+            var initialTouchX : Float = 0.toFloat()
+            var initialTouchY : Float = 0.toFloat()
 
-
+            override fun onTouch(view: View, event: MotionEvent): Boolean{
+                when(event.action){
+                    MotionEvent.ACTION_DOWN -> {
+                        //remember the initial position.
+                        initialX = params.x
+                        initialY = params.y
+                        //get the touch location
+                        initialTouchX = event.rawX
+                        initialTouchY = event.rawY
+                        lastAction = event.action
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        //As we implemented on touch listener with ACTION_MOVE,
+                        //we have to check if the previous action was ACTION_DOWN
+                        //to identify if the user clicked the view or not.
+                        if (lastAction == MotionEvent.ACTION_DOWN) {
+                            //Open the chat conversation click.
+                            val intent = Intent(this@FloatingMenuService, MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                            //close the service and remove the chat heads
+                            stopSelf()
+                        }
+                        lastAction = event.action
+                        return true
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (event.getRawX() - initialTouchX) as Int
+                        params.y = initialY + (event.getRawY() - initialTouchY) as Int
+                        //Update the layout with new X & Y coordinate
+                        mWindowManager.updateViewLayout(floatingMenuView, params)
+                        lastAction = event.action
+                        return true
+                    }
+                }
+                return false
+            }
         }
+
+
+        )
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (floatingMenuView != null) mWindowManager.removeView(floatingMenuView)
+        mWindowManager.removeView(floatingMenuView)
     }
 }
