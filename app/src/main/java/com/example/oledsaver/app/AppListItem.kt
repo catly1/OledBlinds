567ed65dd71d3c7bb4dda7447d8515c9794de85d
Carlos Catly <ccatly@gmail.com>
2020-09-02 19:07:56 -0700

    removed unused fragments/databases. So far nothing is broken. Will proceed on removing dependencies

--- app/src/main/java/com/example/oledsaver/app/AppListItem.kt
+++
@@ -1,5 +0,0 @@
-package com.example.oledsaver.app
-
-import android.graphics.drawable.Drawable
-
-data class AppListItem(var name: String, var icon: Drawable)
--- app/src/main/java/com/example/oledsaver/features/home/HomeViewFragment.kt
+++ app/src/main/java/com/example/oledsaver/features/home/HomeViewFragment.kt
@@ -6,19 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
-import android.widget.Toast
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.oledsaver.R
-import com.example.oledsaver.adapter.SavedAppAdapter
-import com.example.oledsaver.app.AppListItem
-import com.example.oledsaver.entity.Setting
 import com.example.oledsaver.features.floating_menu.FloatingMenuService
-import com.example.oledsaver.features.setting.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_first.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -30,16 +21,11 @@ class HomeViewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fab.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-
         initializeView()
 
     }
