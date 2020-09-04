dc349701dc7036b8b66706618fc6c60f4833d578
Carlos Catly <ccatly@gmail.com>
2020-09-04 16:10:59 -0700

    added custom buttons for the tiles as well

--- app/src/main/java/com/example/oledsaver/features/floating_menu/OLEDSaverTileService.kt
+++ app/src/main/java/com/example/oledsaver/features/floating_menu/OLEDSaverTileService.kt
@@ -1,9 +1,11 @@
 package com.example.oledsaver.features.floating_menu
 
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import androidx.preference.PreferenceManager
+import com.example.oledsaver.R
 
 class OLEDSaverTileService : TileService() {
     private var status = false
@@ -27,9 +29,11 @@ class OLEDSaverTileService : TileService() {
         if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("isActive", false)) {
             //        tile.icon = Icon.createWithResource(this, R.drawable.other_icon)
             tile.state = Tile.STATE_ACTIVE
+            tile.icon = Icon.createWithResource(this, R.drawable.ic_oledsaveron)
             status = true
         } else {
             tile.state = Tile.STATE_INACTIVE
+            tile.icon = Icon.createWithResource(this, R.drawable.ic_oledsaveroff)
             status = false
         }
 
