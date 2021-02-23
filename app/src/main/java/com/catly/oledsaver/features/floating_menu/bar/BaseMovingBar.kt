a16e3caa5b8457ef4e5e8b7d05084d470f14f6db
Carlos Catly <ccatly@gmail.com>
2021-02-22 21:19:15 -0800

    starting base moving bar

---
+++ app/src/main/java/com/catly/oledsaver/features/floating_menu/bar/BaseMovingBar.kt
@@ -0,0 +1,6 @@
+package com.catly.oledsaver.features.floating_menu.bar
+
+import com.catly.oledsaver.features.floating_menu.FloatingMenuService
+
+class BaseMovingBar(floatingMenuService: FloatingMenuService): BaseBar(floatingMenuService) {
+}
