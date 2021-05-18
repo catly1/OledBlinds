b154b452b6e3851f663e800a565deab5b07e2e5b
Carlos Catly <ccatly@gmail.com>
2021-05-17 23:13:01 -0700

    refactored the guide fragment

--- app/src/main/java/com/catly/oledsaver/features/view/GuideFragment.kt
+++ app/src/main/java/com/catly/oledsaver/features/view/GuideFragment.kt
@@ -9,18 +9,4 @@ import androidx.navigation.fragment.findNavController
 import com.catly.oledsaver.R
 import kotlinx.android.synthetic.main.guide_fragment.*
 
-class GuideFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.guide_fragment, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        guide_next_button.setOnClickListener {
-            findNavController().navigate(R.id.action_guideFragment_to_tileGuideFragment)
-        }
-    }
-}
+class GuideFragment : BaseGuideFragment(R.layout.guide_fragment, R.id.action_guideFragment_to_tileGuideFragment) {}
