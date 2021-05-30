9ed59df4b1f058acaac5d75a566ae029fa565e2e
Carlos Catly <ccatly@gmail.com>
2021-05-30 15:35:36 -0700

    refactoring the adapter to use data from the view model

--- app/src/main/java/com/catly/oledsaver/features/view/GuideViewModel.kt
+++ app/src/main/java/com/catly/oledsaver/features/view/GuideViewModel.kt
@@ -1,7 +1,20 @@
 package com.catly.oledsaver.features.view
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.catly.oledsaver.features.data.GuideItemRepository
+import com.catly.oledsaver.features.data.model.GuideIndexItem
+import java.lang.Exception
 
-class GuideViewModel(private val guideItemRepository: GuideItemRepository): ViewModel() {
+class GuideViewModel(guideItemRepository: GuideItemRepository): ViewModel() {
+    val guideList = MutableLiveData<List<GuideIndexItem>>()
+    private val TAG = "GuideViewModel"
+    init {
+        try {
+            guideList.value = guideItemRepository.getGuideIndexItems()
+        } catch (e: Exception){
+            Log.i(TAG,e.toString())
+        }
+    }
 }
