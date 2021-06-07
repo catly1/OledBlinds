e7f3ade4e1f0765476037de307ddb0955059d089
Carlos Catly <ccatly@gmail.com>
2021-06-06 23:20:41 -0700

    RC, proceeding to device testing for one week

--- app/src/main/java/com/catly/oledsaver/features/view/guide/GuideFragment.kt
+++ app/src/main/java/com/catly/oledsaver/features/view/guide/GuideFragment.kt
@@ -1,5 +1,4 @@
 package com.catly.oledsaver.features.view.guide
 import com.catly.oledsaver.R
-import com.catly.oledsaver.features.view.guide.BaseGuideFragment
 
-class GuideFragment : BaseGuideFragment(R.layout.guide_fragment, R.id.action_guideFragment_to_tileGuideFragment) {}
+class GuideFragment : BaseGuideFragment(R.layout.guide_fragment, R.id.action_guideFragment_to_tileGuideFragment)
--- app/src/main/java/com/catly/oledsaver/features/view/guide/TileGuideFragment.kt
+++ app/src/main/java/com/catly/oledsaver/features/view/guide/TileGuideFragment.kt
@@ -1,6 +1,4 @@
 package com.catly.oledsaver.features.view.guide
 import com.catly.oledsaver.R
-import com.catly.oledsaver.features.view.guide.BaseGuideFragment
 
-class TileGuideFragment : BaseGuideFragment(R.layout.tile_guide_fragment, R.id.action_tileGuideFragment_to_homeFragment) {
-}
+class TileGuideFragment : BaseGuideFragment(R.layout.tile_guide_fragment, R.id.action_tileGuideFragment_to_homeFragment)
