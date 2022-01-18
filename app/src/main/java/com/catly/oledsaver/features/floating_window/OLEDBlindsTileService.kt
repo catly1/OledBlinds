2fc1b9de98f1769438c6f9144b186becd4ebca6c
Carlos Catly <ccatly@gmail.com>
2022-01-18 05:20:58 -0800

    added a condition for notification closing when pressing the button on the tile

--- app/src/main/java/com/catly/oledsaver/features/floating_window/OLEDBlindsTileService.kt
+++ app/src/main/java/com/catly/oledsaver/features/floating_window/OLEDBlindsTileService.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.drawable.Icon
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
-import androidx.preference.PreferenceManager
 import com.catly.oledsaver.R
 
 class OLEDBlindsTileService : TileService() {
@@ -18,9 +17,10 @@ class OLEDBlindsTileService : TileService() {
             FloatingWindowService.stopService(this)
             setOffIcon()
         }
-
-        val closeNotificationPanelIntent = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
-        this.sendBroadcast(closeNotificationPanelIntent)
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.S) {
+            val closeNotificationPanelIntent = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+            this.sendBroadcast(closeNotificationPanelIntent)
+        }
     }
 
     override fun onStartListening() {
